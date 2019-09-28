edb99b2c35adf212f5aa3476e5bb4b6717054576
elijahmm17 <elijahmm17@gmail.com>
2019-09-28 19:43:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name);
+    }
+  });
+}
+
+function driversByRevenue(drivers) {
+  const results = [...drivers];
+  results.sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+  return results;
+}
+
+function driversByName(drivers) {
+  const results = [...drivers];
+  results.sort(function(a, b) {
+    return b.name - a.name;
+  });
+  return results;
+}
+
+function totalRevenue(drivers) {
+  
+}
+
+function averageRevenue(drivers) {
+  
+}
