f75aa3cf008194c37b1bb44e255170c8ee269ac9
elijahmm17 <elijahmm17@gmail.com>
2019-09-28 19:44:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function driversByRevenue(drivers) {
 function driversByName(drivers) {
   const results = [...drivers];
   results.sort(function(a, b) {
-    return a.localCompare(b);
+    return a.localeCompare(b);
   });
   return results;
 }
